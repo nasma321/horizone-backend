b97e05ccd58c1b7ac91f5e6b4583a7f429140587
nasma21 <nsmnajeeb@gmail.com>
2025-02-23 22:54:00 +0530

    updates

--- src/index.ts
+++ src/index.ts
@@ -20,5 +20,5 @@ app.use("/api/bookings", bookingsRouter);
 
 app.use(globalErrorHandlingMiddleware);
 
-const PORT = 8000;
+const PORT = 8001;
 app.listen(PORT, console.log(`Server is running on port ${PORT}...`));
