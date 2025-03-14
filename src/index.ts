4dcf9ef7276c6aa6ea116b2b016f397e57f04e57
nasma21 <nsmnajeeb@gmail.com>
2025-03-14 22:21:12 +0530

    backend auth

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,18 @@ import usersRouter from "./api/user";
 import bookingsRouter from "./api/booking";
 import cors from "cors";
 import globalErrorHandlingMiddleware from "./api/middlewares/global-error-handling-middleware";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
+
+app.use(clerkMiddleware());
+
 app.use(express.json());
 app.use(cors());
 
 connectDB();
 
 app.use("/api/hotels", hotelsRouter);
-app.use("/api/users", usersRouter);
 app.use("/api/bookings", bookingsRouter);
 
 app.use(globalErrorHandlingMiddleware);
